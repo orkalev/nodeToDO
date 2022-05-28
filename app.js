cab0511bc7c4b917d264093643686a5c8dbd08b7
Or Kalev <orkalev@gmail.com>
2022-05-28 10:47:52 +0300

    connetMongoDB
    Finish create the DB and the connection for the DB

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var config = require('./config');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine' , 'ejs');
+
+mongoose.connect(config.getDbconnectionString());
 
 app.listen(port);
